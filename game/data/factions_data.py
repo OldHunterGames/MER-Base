8e3d64b13c1e73376a0a5ca26d58688e3bf7b6bc
Old Huntsman <old_huntsman@yahoo.com>
2017-10-05 18:43:02 +0300

    factions data

---
+++ game/data/factions_data.py
@@ -0,0 +1,11 @@
+init python:
+
+    faction_occupations = {
+    'breter': {'name': __('Breter'), 'attribute': 'hardiness',},
+    'poet': {'name': __('Poet'), 'attribute': 'creativity',},   
+    'wisperer': {'name': __('Wisperer'), 'attribute': 'subtlety',},      
+    'engineer': {'name': __('Engineer'), 'attribute': 'competence',},
+    'trainer': {'name': __('Slave trainer'), 'attribute': 'willpower',},
+    'concubine': {'name': __('Concubine'), 'attribute': 'grace',},   
+    }
+
