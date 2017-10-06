09d1e2227a4b1e683638bd89a55d7ac866cdefaa
Old Huntsman <old_huntsman@yahoo.com>
2017-10-06 16:30:37 +0300

    levels descriptions

--- game/data/factions_data.py
+++ game/data/factions_data.py
@@ -1,11 +1,19 @@
 init python:
 
     faction_occupations = {
-    'breter': {'name': __('Breter'), 'attribute': 'hardiness',},
-    'poet': {'name': __('Poet'), 'attribute': 'creativity',},   
-    'wisperer': {'name': __('Wisperer'), 'attribute': 'subtlety',},      
-    'engineer': {'name': __('Engineer'), 'attribute': 'competence',},
-    'trainer': {'name': __('Slave trainer'), 'attribute': 'willpower',},
-    'concubine': {'name': __('Concubine'), 'attribute': 'grace',},   
+    'breter': {'name': __('breter'), 'attribute': 'hardiness',},
+    'poet': {'name': __('poet'), 'attribute': 'creativity',},   
+    'wisperer': {'name': __('wisperer'), 'attribute': 'subtlety',},      
+    'engineer': {'name': __('engineer'), 'attribute': 'competence',},
+    'trainer': {'name': __('slave trainer'), 'attribute': 'willpower',},
+    'concubine': {'name': __('concubine'), 'attribute': 'grace',},   
     }
+
+    faction_occupation_lvl = [__('incompetent'),
+    __('novice'),
+    __('competent'),
+    __('established'),
+    __('elite'),
+    __('famous'),
+    ]
 
