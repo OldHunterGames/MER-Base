20d515b28e39c262c75c7bb5bb9ebaf03d1fb613
Old Huntsman <old_huntsman@yahoo.com>
2017-10-08 19:09:57 +0300

    gender occupatiions

--- game/data/factions_data.py
+++ game/data/factions_data.py
@@ -1,12 +1,13 @@
 init python:
 
     faction_occupations = {
-    'breter': {'name': __('breter'), 'attribute': 'hardiness',},
+    'breter': {'name': __('breter'), 'attribute': 'hardiness',},   
     'artist': {'name': __('artist'), 'attribute': 'creativity',},   
     'wisperer': {'name': __('wisperer'), 'attribute': 'subtlety',},      
     'engineer': {'name': __('engineer'), 'attribute': 'competence',},
     'trainer': {'name': __('slave trainer'), 'attribute': 'willpower',},
-    'concubine': {'name': __('concubine'), 'attribute': 'grace',},   
+    'courtesan': {'name': __('courtesan'), 'attribute': 'grace', 'gender_exclusive': 'feminine'},   
+    'jester': {'name': __('jester'), 'attribute': 'grace', 'gender_exclusive': 'masculine'},       
     }
 
     faction_occupation_lvl = [__('an incompetent'),
