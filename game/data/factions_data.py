bc34c529ae7a4f1c349be42ddee36436a85e0c62
Old Huntsman <old_huntsman@yahoo.com>
2018-03-12 20:30:21 +0300

    slovarij

--- game/data/factions_data.py
+++ game/data/factions_data.py
@@ -1,13 +1,27 @@
 init python:
 
+    occupations = {
+    'vatican': {
+    'guard': {'rank': __('hellebardier', 'korporal', 'wachtmeister', 'feldwebel', 'hauptmann', 'oberstleutnant'), 'attribute': 'hardiness',},
+    'scholar': {'name': __('apprentice', 'scribe', 'registrar', 'lector', 'librarian', 'archivist',), 'attribute': 'competence',},
+    'cleric': {'name': __('novitiate', 'enoch', 'monk', 'cleric', 'vicar', 'abbot',), 'attribute': 'willpower',},
+    'missionary': {'name': __('novice', 'pastor', 'preacher', 'hospeller', 'prophet', 'hierophant',), 'attribute': 'grace'},
+    'chorus': {'name': __('reserve-singer', 'singer', 'chaplain', 'vizekapellmeister', 'kapellmeister', 'choirmaster',), 'attribute': 'creativity',},
+    'inquisitor': {'name': __('apprentice', 'detective', 'custodian', 'inquisitor', 'magister', 'lord-inqusitor',), 'attribute': 'subtlety',},
+    },
+
+    }
+
+
+### УСТАРЕВШАЯ СИСТЕМА
     faction_occupations = {
-    'scout': {'name': __('mist scout'), 'attribute': 'hardiness',},   
-    'artist': {'name': __('artist'), 'attribute': 'creativity',},   
-    'wisperer': {'name': __('wisperer'), 'attribute': 'subtlety',},      
+    'scout': {'name': __('mist scout'), 'attribute': 'hardiness',},
+    'artist': {'name': __('artist'), 'attribute': 'creativity',},
+    'wisperer': {'name': __('wisperer'), 'attribute': 'subtlety',},
     'engineer': {'name': __('engineer'), 'attribute': 'competence',},
     'trainer': {'name': __('slave trainer'), 'attribute': 'willpower',},
-    'courtesan': {'name': __('courtesan'), 'attribute': 'grace', 'gender_exclusive': 'feminine'},   
-    'jester': {'name': __('jester'), 'attribute': 'grace', 'gender_exclusive': 'masculine'},       
+    'courtesan': {'name': __('courtesan'), 'attribute': 'grace', 'gender_exclusive': 'feminine'},
+    'jester': {'name': __('jester'), 'attribute': 'grace', 'gender_exclusive': 'masculine'},
     }
 
     faction_occupation_lvl = [__('an incompetent'),
@@ -17,4 +31,3 @@ init python:
     __('an elite'),
     __('a famous'),
     ]
-
